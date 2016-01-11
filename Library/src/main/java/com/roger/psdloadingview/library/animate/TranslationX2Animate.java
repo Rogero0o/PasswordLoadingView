fcbd700e894573a3f27dc6307a9d1fbf42e0a095
Rogero0o <402435491@qq.com>
2016-01-11 11:25:27 +0800

    update code

--- Library/src/main/java/com/roger/psdloadingview/library/animate/TranslationX2Animate.java
+++ Library/src/main/java/com/roger/psdloadingview/library/animate/TranslationX2Animate.java
@@ -1,21 +1,39 @@
 package com.roger.psdloadingview.library.animate;
 
 import android.graphics.Canvas;
+import com.roger.psdloadingview.library.PsdLoadingView;
+import java.util.ArrayList;
 
 /**
  * Created by Administrator on 2016/1/8.
  */
 public class TranslationX2Animate extends BaseAnimate {
+
+    ArrayList<Float> movexArray;
+
+
+    @Override public void init(PsdLoadingView mPsdLoadingView) {
+        super.init(mPsdLoadingView);
+    }
+
+
     @Override public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         if (!isStop) {
+            if (movexArray == null || movexArray.size() < textLength) {
+                movexArray = new ArrayList<Float>(textLength);
+                for (int i = 0; i < textLength; i++) {
+                    movexArray.add(0.0f);
+                }
+            }
             for (int i = 0; i < textLength; i++) {
-                canvas.drawText(DOT + "", 0, 1, progress *
-                                (mPsdLoadingView.getWidth() -
-                                        (textLength + 2) * distance) +
-                                (i + 1) * distance * (1 +
-                                        (int) Math.abs(progress - 0.5) * 2),
-                        startY, mPaint);
+                if ((float) i < progress * (float) textLength &&
+                        progress * (float) textLength < ((float) i + 1f)) {
+                    movexArray.set(i, progress * (mPsdLoadingView.getWidth() -
+                            (textLength + 2) * distance));
+                }
+                canvas.drawText(DOT + "", 0, 1,
+                        movexArray.get(i) + (i + 1) * distance, startY, mPaint);
             }
         }
     }
